f30e3ef3be325a9ca83a4f809ad82f987d08d7a3
Bozhidara Pachilova <35433937+ddaribo@users.noreply.github.com>
2025-02-21 15:00:08 +0200

    untitled-app (10) updates

---
+++ src/app/child-view-2/child-view-2.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-view-2',
+  imports: [],
+  templateUrl: './child-view-2.component.html',
+  styleUrls: ['./child-view-2.component.scss']
+})
+export class ChildView2Component {}
